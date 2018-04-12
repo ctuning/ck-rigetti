f857f5299dd6f186e71723aeee472c72886187d4
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-04-12 16:16:39 +0100

    Preliminary version of soft:lib.pyquil
    It is able to detect a pyquil module pre-installed into user-space,
    and sets the env variables QVM_API_KEY and QVM_USER_ID
    from PYQUIL_FOREST_API_KEY and PYQUIL_USER_ID respectively,
    but it does not check the compatibility between the detected module and Python.
    
    The resulting env entry is usable, if you run the compatible Python:
        $ ck virtual env --tags=pyquil
        $ python3
            from pyquil.quil import Program
            from pyquil.api import QVMConnection
            from pyquil.gates import CNOT, H
    
            qvm = QVMConnection()
            p = Program(H(0), CNOT(0, 1))
    
            wf = qvm.wavefunction(p)
            print(wf)

---
+++ soft/lib.pyquil/customize.py
@@ -0,0 +1,96 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+
+import os
+
+##############################################################################
+
+def dirs(i):
+    hosd    = i['host_os_dict']
+    macos   = hosd.get('macos','')
+    dirs    = i.get('dirs', [])
+
+    if macos:
+        python_site_packages_dir = os.path.expanduser("~") + "/Library/Python"
+        if os.path.isdir( python_site_packages_dir ):
+            dirs.append( python_site_packages_dir )
+
+    return {'return':0, 'dirs':dirs}
+
+##############################################################################
+
+def setup(i):
+    """
+    Input:  {
+              cfg              - meta of this soft entry
+              self_cfg         - meta of module soft
+              ck_kernel        - import CK kernel module (to reuse functions)
+
+              host_os_uoa      - host OS UOA
+              host_os_uid      - host OS UID
+              host_os_dict     - host OS meta
+
+              target_os_uoa    - target OS UOA
+              target_os_uid    - target OS UID
+              target_os_dict   - target OS meta
+
+              target_device_id - target device ID (if via ADB)
+
+              tags             - list of tags used to search this entry
+
+              env              - updated environment vars from meta
+              customize        - updated customize vars from meta
+
+              deps             - resolved dependencies for this soft
+
+              interactive      - if 'yes', can ask questions, otherwise quiet
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+            }
+
+    """
+
+
+    # Get variables
+    ck=i['ck_kernel']
+
+    iv=i.get('interactive','')
+
+    cus=i.get('customize',{})
+
+    hosd=i['host_os_dict']
+    tosd=i['target_os_dict']
+
+    winh=hosd.get('windows_base','')
+
+    full_path               = cus.get('full_path','')
+    path_lib                = os.path.dirname(full_path)
+    path_install            = os.path.dirname(path_lib)
+
+    env                     = i['env']
+    env_prefix              = cus['env_prefix']
+    env[env_prefix]         = path_install
+    env[env_prefix+'_LIB']  = path_lib
+
+    env['PYTHONPATH']       = path_install + ( ';%PYTHONPATH%' if winh=='yes' else ':${PYTHONPATH}')
+
+    forest_api_key          = os.environ.get('PYQUIL_FOREST_API_KEY','')
+    user_id                 = os.environ.get('PYQUIL_USER_ID','')
+
+    if forest_api_key and user_id:
+        env['QVM_API_KEY']  = forest_api_key
+        env['QVM_USER_ID']  = user_id
+    else:
+        return {'return':1, 'error':'Environment variables PYQUIL_FOREST_API_KEY and PYQUIL_USER_ID should be set!'}
+
+    return {'return':0, 'bat':''}
