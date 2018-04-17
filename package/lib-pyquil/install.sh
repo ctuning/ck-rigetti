9c2c9c42b3679ef37b3448d311446f13512b9545
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-04-17 15:16:20 +0100

    check out a specific tag version of pyquil

--- package/lib-pyquil/install.sh
+++ package/lib-pyquil/install.sh
@@ -13,7 +13,9 @@
 echo "**************************************************************"
 echo "Installing pyQuil package and its dependencies ..."
 
-    # This is where pip2/pip3 will install the modules.
+GIT_SRC_DIR=${INSTALL_DIR}/src
+
+    # This is where pip will install the modules.
     # It has its own funny structure we don't control :
     #
 EXTRA_PYTHON_SITE=${INSTALL_DIR}/python_deps_site
@@ -22,20 +24,22 @@ SHORT_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.ve
 export PACKAGE_LIB_DIR="${EXTRA_PYTHON_SITE}/lib/python${SHORT_PYTHON_VERSION}/site-packages"
 export PYTHONPATH=$PACKAGE_LIB_DIR:$PYTHONPATH
 
-######################################################################################
-echo ""
-echo "Removing '${EXTRA_PYTHON_SITE}' ..."
-rm -rf ${EXTRA_PYTHON_SITE}
 
 ######################################################################################
-GIT_SRC_DIR=${INSTALL_DIR}/src
-echo "Cloning pyQuil into ${GIT_SRC_DIR} ..."
-git clone https://github.com/rigetticomputing/pyquil $GIT_SRC_DIR
+echo ""
+echo "Removing ${EXTRA_PYTHON_SITE} and ${GIT_SRC_DIR} ..."
+rm -rf "${EXTRA_PYTHON_SITE}" "${GIT_SRC_DIR}"
+
+######################################################################################
+echo "Cloning ${PACKAGE_COMMIT} @ pyQuil into ${GIT_SRC_DIR} ..."
+git clone -b $PACKAGE_COMMIT https://github.com/rigetticomputing/pyquil $GIT_SRC_DIR
 
 ######################################################################################
 echo "Installing pyQuil to '${PACKAGE_LIB_DIR}' ..."
 
-${CK_PYTHON_BIN} -m pip install $GIT_SRC_DIR -r $GIT_SRC_DIR/examples/requirements.txt --prefix=${EXTRA_PYTHON_SITE} --no-cache-dir # --ignore-installed
+EXAMPLE_REQ=`ls -1 $GIT_SRC_DIR/examples/requirements.txt 2>/dev/null`
+
+${CK_PYTHON_BIN} -m pip install $GIT_SRC_DIR ${EXAMPLE_REQ:+"--requirement=${EXAMPLE_REQ}"} --prefix=${EXTRA_PYTHON_SITE} --no-cache-dir # --ignore-installed
 
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
