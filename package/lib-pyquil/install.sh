54ac1a3c2c5ef630d1792049cf899d4e1e14d51c
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-04-17 14:33:16 +0100

    perform our own git-clone to have local access to all project files, including examples and tests

--- package/lib-pyquil/install.sh
+++ package/lib-pyquil/install.sh
@@ -16,30 +16,34 @@ echo "Installing pyQuil package and its dependencies ..."
     # This is where pip2/pip3 will install the modules.
     # It has its own funny structure we don't control :
     #
-PY_DEPS_TREE=${INSTALL_DIR}/py_deps
+EXTRA_PYTHON_SITE=${INSTALL_DIR}/python_deps_site
 
-    # This is the link that *will* be pointing at the directory with modules.
-    # However, because we want to use asterisk expansion, we will create
-    # the link itself *after* PY_DEPS_TREE has been already populated.
-    #
-export PACKAGE_LIB_DIR=${INSTALL_DIR}/build
+SHORT_PYTHON_VERSION=`${CK_ENV_COMPILER_PYTHON_FILE} -c 'import sys;print(sys.version[:3])'`
+export PACKAGE_LIB_DIR="${EXTRA_PYTHON_SITE}/lib/python${SHORT_PYTHON_VERSION}/site-packages"
+export PYTHONPATH=$PACKAGE_LIB_DIR:$PYTHONPATH
 
 ######################################################################################
 echo ""
-echo "Removing '${PY_DEPS_TREE}' ..."
-rm -rf ${PY_DEPS_TREE} ${PACKAGE_LIB_DIR}
+echo "Removing '${EXTRA_PYTHON_SITE}' ..."
+rm -rf ${EXTRA_PYTHON_SITE}
+
+######################################################################################
+GIT_SRC_DIR=${INSTALL_DIR}/src
+echo "Cloning pyQuil into ${GIT_SRC_DIR} ..."
+git clone https://github.com/rigetticomputing/pyquil $GIT_SRC_DIR
 
 ######################################################################################
 echo "Installing pyQuil to '${PACKAGE_LIB_DIR}' ..."
 
-${CK_PYTHON_BIN} -m pip install pyquil --prefix=${PY_DEPS_TREE} --no-cache-dir # --ignore-installed
+${CK_PYTHON_BIN} -m pip install $GIT_SRC_DIR -r $GIT_SRC_DIR/examples/requirements.txt --prefix=${EXTRA_PYTHON_SITE} --no-cache-dir # --ignore-installed
 
 if [ "${?}" != "0" ] ; then
   echo "Error: installation failed!"
   exit 1
 fi
 
-    # In order for the asterisk to expand properly,
-    # we have to do it AFTER the directory tree has been populated:
+    # Because we have to provide a fixed name via meta.json ,
+    # and $PACKAGE_LIB_DIR depends on the Python version,
+    # we solve it by creating a symbolic link with a fixed name.
     #
-ln -s $PY_DEPS_TREE/lib/python*/site-packages ${PACKAGE_LIB_DIR}
+ln -s $PACKAGE_LIB_DIR ${INSTALL_DIR}/build
